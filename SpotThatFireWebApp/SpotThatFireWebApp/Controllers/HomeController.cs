e80e3042cdd57db8d92f1aaa08d063829bfcbe15
Enache Ionut <enachestoianionut@gmail.com>
2018-10-20 14:48:05 +0300

    get data from csv

--- SpotThatFireWebApp/SpotThatFireWebApp/Controllers/HomeController.cs
+++ SpotThatFireWebApp/SpotThatFireWebApp/Controllers/HomeController.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using SpotThatFireWebApp.Models;
+using DataLoaderCsv;
 
 namespace SpotThatFireWebApp.Controllers
 {
@@ -26,5 +25,38 @@ namespace SpotThatFireWebApp.Controllers
 
             return View();
         }
+
+
+        #region Get Data
+
+        private List<Fire> GetData()
+        {
+            List<Fire> fireList = new List<Fire>();
+
+            var fires = CsvLoader.Load(@"c:\test\data.csv");
+            foreach (var fire in fires)
+            {
+                fireList.Add(new Fire()
+                {
+                    Brightness = fire.Brightness,
+                    BrightT31 = fire.BrightT31,
+                    Confidence = fire.Confidence,
+                    DayNight = fire.DayNight,
+                    Frp = fire.Frp,
+                    Latitude = fire.Latitude,
+                    Longitude = fire.Longitude,
+                    Satellite = fire.Satellite,
+                    Scan = fire.Scan,
+                    Time = fire.Time,
+                    Track = fire.Track,
+                    Version = fire.Version
+                });
+            }
+
+            return fireList;
+        }
+
+
+        #endregion
     }
 }
