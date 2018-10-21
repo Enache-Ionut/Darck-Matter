7a75662ab30f6469391ab3ace2a10959ad15c554
RaduAlbastroiu <albastroiuradug@gmail.com>
2018-10-21 09:40:37 +0300

    Changed a few lines

--- SpotThatFireIOSApp/SpotThatFire/StubData.swift
+++ SpotThatFireIOSApp/SpotThatFire/StubData.swift
@@ -11,17 +11,23 @@ import CoreLocation
 
 class StubData {
     
-    static func getFireLocations() -> [CLLocation] {
-        var result = [CLLocation]()
-        
+    var data = [CLLocation]()
+    
+    init() {
         var location1 = CLLocation(latitude: CLLocationDegrees(exactly: 44.311)!, longitude: CLLocationDegrees(exactly: 23.819)!)
         var location2 = CLLocation(latitude: CLLocationDegrees(exactly: 44.316)!, longitude: CLLocationDegrees(exactly: 23.790)!)
         var location3 = CLLocation(latitude: CLLocationDegrees(exactly: 44.318)!, longitude: CLLocationDegrees(exactly: 23.798)!)
         
-        result.append(location1)
-        result.append(location2)
-        result.append(location3)
-        
-        return result
+        data.append(location1)
+        data.append(location2)
+        data.append(location3)
+    }
+    
+    func addFireLocation(location: CLLocation) {
+        data.append(location)
+    }
+    
+    func getFireLocations() -> [CLLocation] {
+        return data
     }
 }
--- SpotThatFireIOSApp/SpotThatFire/ViewController.swift
+++ SpotThatFireIOSApp/SpotThatFire/ViewController.swift
@@ -9,18 +9,22 @@
 import UIKit
 import MapKit
 import QuartzCore
+import UserNotifications
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var buttonView: UIView!
     @IBOutlet weak var centerMapView: UIView!
     @IBOutlet weak var centerOnLocationImageView: UIImageView!
     @IBOutlet weak var showFiresOnMapSwitch: UISwitch!
+    @IBOutlet weak var fireReportedView: UIView!
+    @IBOutlet weak var fireReportedLabel: UILabel!
     
     var mapController: MapController?
     var locationManager = LocationManager()
     var networkManager = NetworkManager()
+    var stubData = StubData()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,17 +32,30 @@ class ViewController: UIViewController {
         buttonView.layer.cornerRadius = 10
         buttonView.clipsToBounds = true
         
+        fireReportedView.layer.cornerRadius = 10
+        fireReportedView.clipsToBounds = true
+        fireReportedView.alpha = 0.0
+        fireReportedLabel.alpha = 0.0
+        
         centerMapView.layer.cornerRadius = 25
         centerMapView.clipsToBounds = true
-        
+    
         let centerMapImage = UIImage(named: "target")
         centerOnLocationImageView.image = centerMapImage!
         
         self.mapController = MapController(mapView: mapView)
         locationManager.delegate = mapController
+        mapController?.stubData = stubData
         
         mapController?.networkManager = networkManager
         networkManager.mapView = mapController
+        networkManager.stubData = stubData
+        
+        // double tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.doubleTap(_:)))
+        tapGesture.delegate = self
+        tapGesture.numberOfTapsRequired = 2
+        centerMapView.addGestureRecognizer(tapGesture)
     }
     
     @IBAction func centerMapOnLocation(_ sender: Any) {
@@ -57,11 +74,55 @@ class ViewController: UIViewController {
         }
     }
     
+    func showFireReported() {
+        UIView.animate(withDuration: 1.0, delay: 0.2, options:.curveEaseOut, animations: {
+            self.fireReportedLabel.alpha = 1.0
+            self.fireReportedView.alpha = 1.0
+        }, completion: { _ in
+            self.showFireReported2()
+        })
+    }
+    
+    func showFireReported2() {
+        UIView.animate(withDuration: 1.0, delay: 1.0, options:.curveEaseIn, animations: {
+            self.fireReportedLabel.alpha = 0.0
+            self.fireReportedView.alpha = 0.0
+        }, completion: { _ in
+            self.mapController?.dropPinOnLocation()
+        })
+
+    }
+    
+    func scheduleNotification(timeInSeconds: Double) {
+        let content = UNMutableNotificationContent()
+        content.title = "Fire Alert"
+        content.body = "There is a fire near you, follow the escape route"
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: timeInSeconds, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "demo", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
+    
+    @objc func doubleTap(_ gesture: UITapGestureRecognizer) {
+        self.scheduleNotification(timeInSeconds: 5)
+    }
+    
+    @IBAction func revealLocation(_ sender: UILongPressGestureRecognizer) {
+        let touchLocation = sender.location(in: mapView)
+        let locationCoordinate = mapView.convert(touchLocation, toCoordinateFrom: mapView)
+        let location = CLLocation(latitude: locationCoordinate.latitude, longitude: locationCoordinate.longitude)
+        mapController?.dropPinOnLocation(location: location)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier == "ReportFireSegue") {
             if let destination = segue.destination as? PhotoViewController {
                 destination.currentLocation = locationManager.currentLocation
                 destination.networkManager = networkManager
+                destination.previousController = self
             }
         }
     }
